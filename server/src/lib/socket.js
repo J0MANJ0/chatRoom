eca772844925c0659cf87a20d40a5df08056fe38
Joseph Mark <josephmarkmay@gmail.com>
2025-06-29 11:41:46 +0300

    update routes

--- server/src/lib/socket.js
+++ server/src/lib/socket.js
@@ -7,8 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin:
-      process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : '/',
+    origin: ['http://localhost:5173'],
     credentials: true,
   },
 });
